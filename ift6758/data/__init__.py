78f72b2a6d93a7e8cacfe1d1ddb6a726e3edff10
Mohamed Amin Soumih <mohamedaminsoumih@Mohameds-MacBook-Pro.local>
2024-10-01 17:13:28 -0400

    Supprimer le dossier Widgets

--- ift6758/data/__init__.py
+++ ift6758/data/__init__.py
@@ -1,14 +1,86 @@
-"""
-You can "pre" import stuff in the __init__.py module, but this is not required.
-This allows you to import this function as:
+import pandas as pd
+import requests
+import json
+import os
 
-    from ift6758.data import get_player_stats
+class DataRetriever:    
+    # 30 teams -> 1230
+    # 31 team -> 1271
+    # 32 team -> 1353
+    
+    def __init__(self):
+        self.GAME_TYPES = ['02', '03']
+        # self.GAME_TYPES = ['01', '02', '03']
+        self.GAME_TYPE_NAMES = ['season', 'playoffs']
+        # self.GAME_TYPE_NAMES = ['preseason', 'season', 'playoffs']
+        self.GAME_COUNTS = {
+            '2016': 1230,
+            '2017': 1271, 
+            '2018': 1271,
+            '2019': 1082, # 1271
+            '2020': 869,  # 1271
+            '2021': 1353, # COVID-19
+            '2022': 1353, # 1313 (exact)
+            '2023': 1353,
+            '2024': 1353, # 1313 (exact)
+        }
+        self.PLAYOFFS_ROUNDS = 4
+        self.PLAYOFFS_GAMES = 7
 
-instead of only:
+    def prepare(self):
+        for season in self.GAME_COUNTS.keys():
+            os.makedirs(f'ift6758/data/{season}', exist_ok=True)
+    
+    def get_data(self):
+        for season in data_obj.GAME_COUNTS.keys():
+            data_obj.get_games_for_season(season)
+            data_obj.get_playoffs_games_for_season(season)
+            
+    def get_playoffs_game(self, season, round_id, matchup_id, game_id):
+        response = requests.get(f'https://api-web.nhle.com/v1/gamecenter/{season}030{round_id}{matchup_id}{game_id}/play-by-play')
+        print(f'{season}03{round_id}{matchup_id}{game_id}\t', response.status_code)
+        data = {}
+        if response.status_code == 200:
+            data = response.json()
+        return response.status_code, data
+            
+    def get_season_game(self, season, game_type_index, formatted_game_id):
+        response = requests.get(f'https://api-web.nhle.com/v1/gamecenter/{season}{self.GAME_TYPES[game_type_index]}{formatted_game_id}/play-by-play')
+        print(f"{season}{self.GAME_TYPES[game_type_index]}{formatted_game_id}\t", response.status_code)
+        data = {}
+        if response.status_code == 200:
+            data = response.json()
+        return response.status_code, data
 
-    from ift6758.data.question_1 import get_player_stats
+    def get_games_for_season(self, season):
+        data = []
+        for game_id in range(1, self.GAME_COUNTS[season] + 1):
+            formatted_game_id = str(game_id).zfill(4)
+            game_data = self.get_season_game(season, 0, formatted_game_id)
+            if game_data[0] == 404:
+                with open(f'ift6758/data/{season}/{self.GAME_TYPE_NAMES[0]}.json', 'w') as f:
+                    json.dump(data, f)
+                return
+            data.append(game_data[1])
+        with open(f'ift6758/data/{season}/{self.GAME_TYPE_NAMES[0]}.json', 'w') as f:
+            json.dump(data, f)
 
-but both are still valid. You can read more about packages and modules here:
-https://docs.python.org/3/reference/import.html#regular-packages
-"""
-from .question_1 import get_player_stats
+    def get_playoffs_games_for_season(self, season):
+        # ROUNDS -> 1 - 4
+        # MATCHUPS -> 1 - 8 | 1 - 4 | 1 - 2 | 1
+        # GAME -> 1 - 7
+        data = []
+        for round_id in range(1, self.PLAYOFFS_ROUNDS + 1):
+            matchup_nb = 2 ** (4 - round_id)
+            for matchup_id in range(1, matchup_nb + 1):
+                for game_id in range(1, self.PLAYOFFS_GAMES + 1):
+                    game_data = self.get_playoffs_game(season, round_id, matchup_id, game_id)
+                    if game_data[0] == 200:
+                        data.append(game_data[1])
+        with open(f'ift6758/data/{season}/{self.GAME_TYPE_NAMES[1]}.json', 'w') as f:
+            json.dump(data, f)
+
+if __name__ == "__main__":
+    data_obj = DataRetriever()
+    data_obj.prepare()
+    data_obj.get_data()
