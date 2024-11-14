a21c86afe5f6c2f8114ee30b1510e3c8805ec724
lofeodo <daniel.lofeodo@gmail.com>
2024-11-14 17:06:19 -0500

    optimize the function to speed it up

--- ift6758/data/data_cleaner_milestone2.py
+++ ift6758/data/data_cleaner_milestone2.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import json
 import os
-import warnings
 from tqdm import tqdm
 
 class DataCleanerMilestone2:
@@ -18,21 +17,13 @@ class DataCleanerMilestone2:
         ]
 
     def clean_data_for_season(self, season, season_type):
-        #? situation_type :
-        # 0 -> PK (powerkill)
-        # 1 -> force egale 
-        # 2 -> PP (powerplay)
         path = f'ift6758/data/{season}/{season_type}.json'
-        df = pd.DataFrame(columns=['period', 'is_goal', 'x_coord', 'y_coord', 
-                                   'shot_type', 'is_empty_net', 'distance', 'angle_to_goal', 
-                                   'game_seconds', 'last_event_type', 'last_event_x_coord', 
-                                   'last_event_y_coord', 'time_since_last_event', 'distance_from_last_event'])
-        
         with open(path, 'r', encoding='utf-8') as f:
             data = json.load(f)
             games_iter = tqdm(data)
             shotcount = 0
             skippedshots = 0
+            rows = []
             for game in games_iter:
                 games_iter.set_description_str("Game: %s" % game['id'])
 
@@ -56,7 +47,7 @@ class DataCleanerMilestone2:
 
                     situation = str(shot['situationCode'])
                     is_away_team = away_team_id == event_owner_team
-                    distance = np.sqrt((x_coord - 90)**2 + y_coord**2)
+                    distance = np.hypot(x_coord - 90, y_coord)
                     angle_to_goal = np.degrees(np.arctan2(y_coord, 90 - x_coord))
                     is_away_goalie_in_net = int(situation[0])
                     is_home_goalie_in_net = int(situation[3])
@@ -66,58 +57,31 @@ class DataCleanerMilestone2:
                     time_in_period_seconds = self._convert_to_seconds(shot['timeInPeriod'])
                     game_seconds = (int(period) - 1) * 20 * 60 + time_in_period_seconds
 
-                    # Previous shot info
                     play_index = game['plays'].index(shot)
-                    if play_index == 0:
-                        last_event_type = None
-                        last_event_x_coord = None
-                        last_event_y_coord = None
-                        time_since_last_event = None
-                        distance_from_last_event = None
-                    else:
-                        previous_play = game['plays'][play_index - 1]
-                        # Only consider the play if it was in the same period
-                        if period == previous_play['periodDescriptor']['number']:
-                            try:
-                                last_event_x_coord = previous_play['details']['xCoord']
-                                last_event_y_coord = previous_play['details']['yCoord']
-                                distance_from_last_event = np.sqrt((x_coord - last_event_x_coord) ** 2 + (y_coord - last_event_y_coord) ** 2)
-                            except KeyError as e:
-                                last_event_x_coord = None
-                                last_event_y_coord = None
-                                distance_from_last_event = None
-
-                            last_event_type = previous_play['typeCode']
-                            time_in_period_seconds = self._convert_to_seconds(previous_play['timeInPeriod'])
-                            last_event_seconds = (int(period) - 1) * 20 * 60 + time_in_period_seconds
-                            time_since_last_event = game_seconds - last_event_seconds
-                        else:
-                            last_event_type = None
-                            last_event_x_coord = None
-                            last_event_y_coord = None
-                            time_since_last_event = None
-                            distance_from_last_event = None
+                    last_event_info = self._get_last_event_info(game, play_index, period, game_seconds, x_coord, y_coord)
 
                     row = {
-                        'period': period,
-                        'is_goal': is_goal,
-                        'x_coord': x_coord,
+                        'period': period, 
+                        'is_goal': is_goal, 
+                        'x_coord': x_coord, 
                         'y_coord': y_coord,
-                        'shot_type': shot_type,
-                        'is_empty_net': int(is_empty_net),
+                        'shot_type': shot_type, 
+                        'is_empty_net': int(is_empty_net), 
                         'distance': distance,
-                        'angle_to_goal': angle_to_goal,
-                        'game_seconds': game_seconds,
-                        'last_event_type': last_event_type,
-                        'last_event_x_coord': last_event_x_coord,
-                        'last_event_x_coord': last_event_y_coord,
-                        'time_since_last_event': time_since_last_event,
-                        'distance_from_last_event': distance_from_last_event
+                        'angle_to_goal': angle_to_goal, 
+                        'game_seconds': game_seconds, 
+                        **last_event_info
                     }
-                    df = df._append(row, ignore_index=True)
+                    rows.append(row)
 
             print(f"Total shots in season: {shotcount}, skipped shots: {skippedshots}, % shots skipped: {skippedshots/shotcount * 100}")
-            out_path = f'ift6758/data/milestone2/{season}/{season_type}.csv'
+            
+            out_dir = f"ift6758/data/milestone2/{season}"
+            if not os.path.exists(out_dir):
+                os.makedirs(out_dir)
+            out_path = f"{out_dir}/{season_type}.csv"
+
+            df = pd.DataFrame(rows)
             df.to_csv(out_path, index=False)
 
     def clean_data(self):
@@ -128,6 +92,43 @@ class DataCleanerMilestone2:
             for game_type in types_iter:
                 types_iter.set_description_str("Type: %s" % game_type)
                 self.clean_data_for_season(season, game_type)
+
+    def _get_last_event_info(self, game, play_index, period, game_seconds, x_coord, y_coord):
+        if play_index == 0:
+            return {
+                'last_event_type': None, 
+                'last_event_x_coord': None, 
+                'last_event_y_coord': None,
+                'time_since_last_event': None, 
+                'distance_from_last_event': None
+                }
+
+        previous_play = game['plays'][play_index - 1]
+        if period != previous_play['periodDescriptor']['number']:
+            return {
+                'last_event_type': None, 
+                'last_event_x_coord': None, 
+                'last_event_y_coord': None,
+                'time_since_last_event': None, 
+                'distance_from_last_event': None
+                }
+
+        last_event_details = previous_play.get('details', {})
+        last_event_x = last_event_details.get('xCoord')
+        last_event_y = last_event_details.get('yCoord')
+        if last_event_x is not None and last_event_y is not None:
+            distance_from_last_event = np.hypot(x_coord - last_event_x, y_coord - last_event_y)
+        else:
+            distance_from_last_event = None
+        last_event_seconds = (int(period) - 1) * 20 * 60 + self._convert_to_seconds(previous_play['timeInPeriod'])
+        
+        return {
+            'last_event_type': previous_play['typeCode'],
+            'last_event_x_coord': last_event_x,
+            'last_event_y_coord': last_event_y,
+            'time_since_last_event': game_seconds - last_event_seconds,
+            'distance_from_last_event': distance_from_last_event
+        }
 
     def _convert_to_seconds(self, time: str):
         time_arr = time.split(':')
@@ -142,11 +143,5 @@ class DataCleanerMilestone2:
         return seconds
 
 if __name__ == '__main__':
-    # Some values are None. This warning filter is required to remove spam in the output regarding null values
-    warnings.simplefilter(action='ignore', category=FutureWarning)
-
-    if not os.path.exists("ift6758/data/milestone2"):
-        os.makedirs("ift6758/data/milestone2")
-
     data_cleaner_obj = DataCleanerMilestone2()
     data_cleaner_obj.clean_data()
