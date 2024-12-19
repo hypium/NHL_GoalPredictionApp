b153f340b9e4e02fccc34523ffe59f22428d28df
lofeodo <daniel.lofeodo@gmail.com>
2024-12-18 21:10:23 -0500

    remove useless variable from serving_client

--- ift6758/ift6758/client/serving_client.py
+++ ift6758/ift6758/client/serving_client.py
@@ -8,13 +8,9 @@ logger = logging.getLogger(__name__)
 
 
 class ServingClient:
-    def __init__(self, ip: str = "0.0.0.0", port: int = 5000, features=None):
+    def __init__(self, ip: str = "0.0.0.0", port: int = 5000):
         self.base_url = f"http://{ip}:{port}"
         logger.info(f"Initializing client; base URL: {self.base_url}")
-
-        if features is None:
-            features = ["distance"]
-        self.features = features
 
 
     def predict(self, X: pd.DataFrame) -> pd.DataFrame:
