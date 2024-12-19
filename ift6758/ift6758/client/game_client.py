aa9e02d8cbe9d2e81cc8300a88989f5c550ca43b
lofeodo <daniel.lofeodo@gmail.com>
2024-12-18 21:50:09 -0500

    add game_client

---
+++ ift6758/ift6758/client/game_client.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import requests
+from serving_client import ServingClient
+
+class GameClient:
+    def __init__(self, model: str = "base_distance"):
+        # event_map contains key value pairs where the keys are the event id
+        # and the values are the goal probabilities for that event.
+        self.event_map = {}
+        self.servingClient = ServingClient(ip="127.0.0.1", port=8000)
+        self.workspace = "IFT6758-2024-A02/IFT6758.2024-A02"
+        self.set_model(model=model)
+
+
+    def fetch_game_data(self, game_id):
+        response = requests.get(f'https://api-web.nhle.com/v1/gamecenter/{game_id}/play-by-play')
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+        
+
+    def ping_server(self, game_id):
+        game_data = self.fetch_game_data(game_id)
+        for play in game_data['plays']:
+            event_id = play['eventId']
+            if play["typeCode"] not in {505, 506} or event_id in self.event_map:
+                # only consider shots and unhandled events
+                continue
+
+            try:
+                x_coord = np.abs(play['details']['xCoord'])
+                y_coord = play['details']['yCoord']
+            except:
+                # The data does not contain the required details, we skip over this shot
+                continue
+
+            distance = np.hypot(x_coord - 90, y_coord)
+            angle_to_goal = np.degrees(np.arctan2(y_coord, 90 - x_coord))
+            df = pd.DataFrame([{'distance': distance, 'angle_to_goal': angle_to_goal}])
+            
+            # There should only be a single probability in the output
+            prediction = self.servingClient.predict(X=df)
+            goal_proba = prediction.loc[0, 'goal_proba']
+            
+            self.event_map[event_id] = goal_proba
+        
+        return self.event_map
+
+    
+    def set_model(self, model: str):
+        self.servingClient.download_registry_model(workspace=self.workspace, 
+                                                   model=model, version="v2")
+            
+
